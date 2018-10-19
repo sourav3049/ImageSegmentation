4f52403350b3bde6b2fbc9416e6e30f789d1d317
sourav3049 <44297819+sourav3049@users.noreply.github.com>
2018-10-19 21:15:29 +0530

    Create losses.py

---
+++ Utils/losses.py
@@ -0,0 +1,34 @@
+# This files contains various type of loss functions 
+# The functions has been developed from multiple sources. References will be added soon
+
+#focal loss
+def focal_loss_fixed(y_true, y_pred):
+    gamma=2.0
+    alpha=0.2
+    pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+    pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+
+#Dice loss calculation
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+
+def bce_dice_loss(y_true, y_pred):
+    return (binary_crossentropy(y_true, y_pred) + 2*dice_loss(y_true, y_pred))
+
+def focal_dice_loss(y_true, y_pred):
+    return (focal_loss_fixed(y_true, y_pred) + 2*dice_loss(y_true, y_pred))
